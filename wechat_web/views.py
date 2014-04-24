9db44845c2ed9418da2406e54b6243ced5a53aec
Yu Jiang <sunlightt07@gmail.com>
2014-04-24 21:10:31 +0800

    change to apache

--- wechat_web/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import Http404, HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate
-import time
-
-def login(request):
-    time.sleep(3)
-    return HttpResponse("hello world!", content_type="text/plain")
-
