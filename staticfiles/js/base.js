dd53dd7c9c8e03a51e1fced4d12cb7640d536f91
Yu Jiang <sunlightt07@gmail.com>
2014-04-24 20:58:51 +0800

    change to zepto

--- staticfiles/js/base.js
+++ staticfiles/js/base.js
@@ -1,2 +1,3 @@
- $('head').find("meta[name=viewport]").attr('content', 'width=device-width, initial-scale=' + $(window).width() / 640 + ', maximum-scale=' + $(window).width() / 640 + ', minimum-scale=' + $(window).width() / 640 + ', user-scalable=0">');
- 
+$('head').find("meta[name=viewport]").attr('content', 'width=device-width, initial-scale=' + $(window).width() / 640 + ', maximum-scale=' + $(window).width() / 640 + ', minimum-scale=' + $(window).width() / 640 + ', user-scalable=0">');
+jQuery = Zepto;
+
