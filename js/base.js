2654718c013e3a2b963328ae34edf73e34995e38
hejian <jianhe25@gmail.com>
2014-06-05 22:30:49 +0800

    fix list.html and mylist.html

--- js/base.js
+++ js/base.js
@@ -1,3 +1,2 @@
-$('head').find("meta[name=viewport]").attr('content', 'width=device-width, initial-scale=' + $(window).width() / 640 + ', maximum-scale=' + $(window).width() / 640 + ', minimum-scale=' + $(window).width() / 640 + ', user-scalable=0');
 jQuery = Zepto;
 
